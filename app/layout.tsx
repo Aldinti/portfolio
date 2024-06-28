d1a66bf4049795039f5209e6515b82094bf3aa1e
Aldinti <aldopati@gmail.com>
2024-06-28 00:52:23 -0500

    change font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Space_Mono, Source_Code_Pro } from "next/font/google";
+import { Space_Mono, Source_Code_Pro, Montserrat_Alternates } from "next/font/google";
 
-const spaceMono = Space_Mono({ weight: ["400", "700"], subsets: ["latin"] });
-const sourceCodePro = Source_Code_Pro({ weight: ["400", "700"], subsets: ["latin"] });
+const sourceFont = Montserrat_Alternates({ weight: ["400", "700"], subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Aldinti's Portfolio",
@@ -13,7 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({children,}: {children: React.ReactNode;}) {
   return (
     <html lang="en">
-      <body className={sourceCodePro.className}>{children}</body>
+      <body className={sourceFont.className}>{children}</body>
     </html>
   );
 }
